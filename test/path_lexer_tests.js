9f1b52cb1d619b70273d9de530d7b47e8f538794
Kevin Lindsey <kevin@kevlindev.com>
2017-07-22 17:13:41 -0700

    Update unit tests to use mocha

--- test/path_lexer_tests.js
+++ test/path_lexer_tests.js
@@ -1,16 +1,20 @@
-var PathLexer = require('../lib/PathLexer');
+let assert = require('assert'),
+    PathLexer = require('../lib/PathLexer');
 
-exports.lex = function(beforeExit, assert) {
-    var lexer = new PathLexer();
-    var pathData = "M50,120 Q100,20 150,120 L250,120 C300,20 350,120 400,20";
+describe('Lexer', () => {
+    it('getNextToken', () => {
+        let lexer = new PathLexer();
+        let pathData = "M50,120 Q100,20 150,120 L250,120 C300,20 350,120 400,20";
 
-    lexer.setPathData(pathData);
+        lexer.setPathData(pathData);
 
-    token = lexer.getNextToken();
-
-    while ( !token.typeis(3) ) {
-        console.log(token.type + ":" + token.text);
-
+        // loop through all tokens
         token = lexer.getNextToken();
-    }
-};
+
+        while ( !token.typeis(3) ) {
+            console.log(token.type + ":" + token.text);
+
+            token = lexer.getNextToken();
+        }
+    })
+});
--- test/path_parser_tests.js
+++ test/path_parser_tests.js
@@ -1,10 +1,13 @@
-var PathParser = require('../lib/PathParser');
-var SampleHandler = require('../lib/SampleHandler');
+let assert = require('assert'),
+    PathParser = require('../lib/PathParser'),
+    SampleHandler = require('../lib/SampleHandler');
 
-exports.parse = function(beforeExit, assert) {
-    var parser = new PathParser();
-    var pathData = "M40,70 Q50,150 90,90 T135,130 L160,70 C180,180 280,55 280,140 S400,110 290,100";
+describe('Parser', () => {
+    it('parseData', () => {
+        let parser = new PathParser();
+        let pathData = "M40,70 Q50,150 90,90 T135,130 L160,70 C180,180 280,55 280,140 S400,110 290,100";
 
-    parser.setHandler(new SampleHandler());
-    parser.parseData(pathData);
-};
+        parser.setHandler(new SampleHandler());
+        parser.parseData(pathData);
+    })
+});
